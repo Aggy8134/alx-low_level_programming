6b541fed97ba74fb2d8f4274dab91524ae1562ef
Aggy8134 <agnesmwangi8134@gmail.com>
2023-02-16 10:35:39 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *main - prints "Programming is like building a multilingual puzzle",
- *         followed by a newline
+ *followed by a newline
  *
  *This function uses the puts function to print a message to the standard
  *output stream.
