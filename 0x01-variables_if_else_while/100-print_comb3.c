16997750b279253cb290dcc9c9803ad4955c9b3a
Aggy8134 <agnesmwangi8134@gmail.com>
2023-02-17 15:43:25 -0800

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+  
+ /** 
+  *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01) 
+  * 
+  *Return: Always 0 (Success) 
+  */ 
+  
+ int main(void) 
+ { 
+         int d, p;
+
+	 for (d = '0'; d < '9'; d++)
+	{
+	
+	for (p = d + 1; p <= '9';)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
