43e9c8a2823aa273d8007091e01367d80eb32a5c
Aggy8134 <agnesmwangi8134@gmail.com>
2023-02-17 14:41:48 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,13 @@
 
 int main(void)
 {
-	char lower = 'a';
-	char upper = 'A';
-
-	while (lower <= 'z') /print lowercases a-z/
-	{
-		putchar(lower);
-		lower++;
-	}
-
-	while (upper <= 'Z') /print uppercase A-Z/
-	{
-		putchar(upper);
-		upper++;
-	}
-
-	putchar('\n');
+	char low;
+	
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+	
+	for(low) = 'A'; low <= 'Z'; low++)
+	putchar(low);
 
 	return (0);
 }
